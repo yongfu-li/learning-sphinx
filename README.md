37a0ede44c099687e4ffd6ca124b17354feacff0
Yongfu Li <liyongfu.sg@gmail.com>
2018-01-14 14:44:42 +0800

    first build

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # learning-sphinx
 This is a learning repository for my sphinx
+
+## Commands
+````
+sphinx-quickstart
+sphinx-build -b html sourcedir builddir
+make html
+````
+
+## References
+* http://www.sphinx-doc.org/en/stable/tutorial.html
+* https://matplotlib.org/sampledoc
+
---
+++ docs/www.sphinx-doc.org/en/stable/_static/readthedocs-data.js
@@ -0,0 +1,14 @@
+  var READTHEDOCS_DATA = {
+    project: "sphinx",
+    version: "stable",
+    language: "en",
+    subprojects: {},
+    canonical_url: "http://www.sphinx-doc.org/en/stable/",
+    theme: "sphinx13",
+    builder: "sphinx",
+    docroot: "/doc/",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    commit: "695b9f9e"
+  };
+  
